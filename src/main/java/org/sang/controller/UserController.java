dc2b1ad768689c7ecdbaccd374963835ffa4a9c1
chuxia <1429068686@qq.com>
2019-10-28 21:38:48 +0800

    提交测试

--- src/main/java/org/sang/controller/UserController.java
+++ src/main/java/org/sang/controller/UserController.java
@@ -1,8 +1,20 @@
 package org.sang.controller;
 
+import org.sang.model.entity.User;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController(value = "api/user")
 public class UserController {
+    @PostMapping("/login")
+    public  String login(Model model){
+        User user = new User();
+        user.setName("123");
+        user.setAge(18);
+        user.setSex("m");
+        model.addAttribute("user",user);
+        return "user";
+    }
 
 }
